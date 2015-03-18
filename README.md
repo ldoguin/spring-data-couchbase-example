a080b307eb717627a209f63856c39cb217344f47
Laurent Doguin <laurent.doguin@couchbase.com>
2015-03-18 11:20:37 +0100

    default conf, commandline runner, first set/get to couchbase

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # spring-data-couchbase-example
 
-Spring Data Couchbase example composed of several steps introducing every features available.
+Spring Data Couchbase example composed of several steps introducing every features available. The goal is to get some
+data out of twitter and put them in Couchbase.
+
+## Step 0
+
+This should give you a ready to start project with the right dependencies so you won't be bother by this aspect. You
+start with Spring Boot, Spring Data Couchbase and Spring Social Twitter.
+
+## Step 1 Configuration
+
+Configuration ready + CommandLine Runner to test Bean Injection, and the couchbase configuration
+
+
--- src/main/java/org/couchbase/advocacy/metrics/Application.java
+++ src/main/java/org/couchbase/advocacy/metrics/Application.java
@@ -1,10 +1,65 @@
 package org.couchbase.advocacy.metrics;
 
-public class Application {
+import com.couchbase.client.CouchbaseClient;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.List;
 
+@Configuration
+@EnableAutoConfiguration
+public class Application extends AbstractCouchbaseConfiguration {
+
+    private final static Log log = LogFactory.getLog(Application.class);
+
+    @Value("${couchbase.cluster.bucket:default}")
+    private String bucketName;
+
+    @Value("${couchbase.cluster.password:}")
+    private String password;
+
+    @Value("${couchbase.cluster.ip:127.0.0.1}")
+    private String ip;
+
+    @Override
+    protected List<String> bootstrapHosts() {
+        return Arrays.asList(ip);
+    }
+
+    @Override
+    protected String getBucketName() {
+        return bucketName;
+    }
+
+    @Override
+    protected String getBucketPassword() {
+        return password;
     }
 
 
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+        ctx.close();
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CouchbaseClient couchbaseClient) {
+        return args -> {
+            couchbaseClient.add("aKey","{'json':'object'}");
+            Object aKey = couchbaseClient().get("aKey");
+            log.info(aKey);
+        };
+    }
 }
+
+
+
